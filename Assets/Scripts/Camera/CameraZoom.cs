8dbd0cf11be803b2a9dc702decb87d8c0e778f81
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-03-05 21:41:03 +0900

    Feat: 카메라 줌 인/아웃 기능 추가
    - 마우스 휠로 카메라 줌 인/아웃 가능

---
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraZoom : MonoBehaviour
+{
+    private Camera _camera;
+    [SerializeField] float zoomSpeed = 2f;
+    [SerializeField] float minZoom = 5f;
+    [SerializeField] float maxZoom = 15f;
+    private float targetZoom;
+    private float zoomVelocity;
+    [SerializeField] float zoomSmoothTime = 0.1f;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        targetZoom = _camera.fieldOfView;
+    }
+
+    public void OnZoom(InputAction.CallbackContext context)
+    {
+        float zoomInput = context.ReadValue<float>();
+        targetZoom -= zoomInput * zoomSpeed;
+        targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
+    }
+
+    private void Update()
+    {
+        _camera.fieldOfView = Mathf.SmoothDamp(_camera.fieldOfView, targetZoom, ref zoomVelocity, zoomSmoothTime);
+    }
+}
