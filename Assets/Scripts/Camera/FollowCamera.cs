a99632cf4125bab1fb81e8a73861ffb36f951392
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-03-06 16:35:06 +0900

    Feat: 카메라 벽 뚫기 막는 레이캐스트 추가
    - 카메라와 플레이어 사이에 뭐 있으면 거기로 카메라 옮김

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -7,10 +7,13 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform cameraContainer;
 
+
     [SerializeField] private Vector3 offset = new Vector3(0, 7f, -10f);
     [SerializeField] private Vector3 defaultRotation = new Vector3(30f, 0f, 0f);
     [SerializeField] private float followSpeed = 5f;
     [SerializeField] private float smoothTime = 0.1f;
+
+    [SerializeField] private LayerMask collisionMask;
     private Vector3 velocity = Vector3.zero;
 
     private void Start()
@@ -26,5 +29,21 @@ public class FollowCamera : MonoBehaviour
         Vector3 targetPosition = player.position;
 
         cameraContainer.position = Vector3.Lerp(cameraContainer.position, targetPosition, Time.fixedDeltaTime * followSpeed);
+
+        // 플레이어와 카메라 사이에 장애물이 있는지 레이캐스트로 확인ㅇㅇ
+        Vector3 desiredCameraPos = cameraContainer.TransformPoint(offset);
+        Vector3 direction = desiredCameraPos - player.position;
+        float distance = offset.magnitude;
+
+        if (Physics.Raycast(player.position, direction.normalized, out RaycastHit hit, distance, collisionMask))
+        {
+            // 장애물이 있으면 장애물 바로 앞에 카메라 세팅
+            transform.position = hit.point + hit.normal * 0.2f;
+        }
+        else
+        {
+            // 뭐 없으면 다시 위치로 옴
+            transform.position = Vector3.SmoothDamp(transform.position, desiredCameraPos, ref velocity, smoothTime);
+        }
     }
 }
