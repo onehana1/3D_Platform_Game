44df8a6c47e70507fe0d93b43f4b0ed901ba56ab
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-03-09 14:17:35 +0900

    Feat: 아이템 장착시 플레이어에 프리팹 추가

--- Assets/Scripts/Player/PlayerEquipment.cs
+++ Assets/Scripts/Player/PlayerEquipment.cs
@@ -19,6 +19,14 @@ public class PlayerEquipment : MonoBehaviour
     public ItemData equipBody;
     public ItemData equipWeapon;
 
+    private GameObject currentWeaponObject;
+    private GameObject currentHeadObject;
+    private GameObject currentBodyObject;
+
+    public Transform headPos;
+    public Transform bodyPos;
+    public Transform weaponPos;
+
 
     public void EquipItem(ItemData item)
     {
@@ -30,14 +38,17 @@ public class PlayerEquipment : MonoBehaviour
             case EquipSlotType.Head:
                 UnequipItem(equipHead);
                 equipHead = item;
+                EquipObject(ref currentHeadObject, headPos, item.equipPrefabs);
                 break;
             case EquipSlotType.Body:
                 UnequipItem(equipBody);
                 equipBody = item;
+                EquipObject(ref currentBodyObject, bodyPos, item.equipPrefabs);
                 break;
             case EquipSlotType.Weapon:
                 UnequipItem(equipWeapon);
                 equipWeapon = item;
+                EquipObject(ref currentWeaponObject, weaponPos, item.equipPrefabs);
                 break;
         }
 
@@ -65,6 +76,30 @@ public class PlayerEquipment : MonoBehaviour
         InventoryManager.Instance.SetEquippedState(item, false);
         FindObjectOfType<UIEquipment>()?.UnequipItem(item.equipSlotType);
     }
+
+    private void EquipObject(ref GameObject currentObject, Transform parentPos, GameObject prefab)
+    {
+        if (prefab == null) return;
+
+        // 기존 장착된 아이템 제거
+        RemoveObject(ref currentObject);
+
+        // 새로운 장비 생성 및 장착
+        currentObject = Instantiate(prefab, parentPos.position, parentPos.rotation);
+        currentObject.transform.SetParent(parentPos); 
+        currentObject.transform.localPosition = Vector3.zero; 
+        currentObject.transform.localRotation = Quaternion.identity;
+    }
+
+    private void RemoveObject(ref GameObject currentObject)
+    {
+        if (currentObject != null)
+        {
+            Destroy(currentObject);
+            currentObject = null;
+        }
+    }
+
 }
 
 
