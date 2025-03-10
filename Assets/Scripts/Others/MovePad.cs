6e8349cf994c19fd9aeac34f334d6d4b847aeb35
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-03-10 21:47:32 +0900

    Feat: 움직이는 플랫폼 추가

---
+++ Assets/Scripts/Others/MovePad.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovePadType { Horizontal, Vertical }
+public class MovePad : MonoBehaviour
+{
+    [Header("Move Settings")]
+    public MovePadType moveType = MovePadType.Horizontal; // 이동 타입 선택
+    public float moveDistance = 3f; 
+    public float moveSpeed = 2f; 
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;  // 시작 위치 저장
+    }
+
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    void MovePlatform()
+    {
+        float offset = Mathf.PingPong(Time.time * moveSpeed, moveDistance * 2) - moveDistance;
+
+        if (moveType == MovePadType.Horizontal)
+            transform.position = startPos + new Vector3(offset, 0, 0);  // 좌우 이동
+        else
+            transform.position = startPos + new Vector3(0, offset, 0);  // 상하 이동
+    }
+}
